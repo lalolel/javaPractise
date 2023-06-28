b27a524c01af4e1844ee64c025df03b541454ca1
lauris7701 <laurislenca@gmail.com>
2023-06-28 20:33:48 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+// Press Shift+F9 to start debugging your code. We have set one breakpoint
+// for you, but you can always add more by pressing Ctrl+F8.
+
+public class Main {
+    public static void main(String[] args) {
+
+      Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter your e-mail: ");
+        String usr_email = scan.nextLine();
+
+        System.out.println("Enter your password: ");
+        String usr_paasword = scan.nextLine();
+
+        if (usr_email.equals("m@m.lv") && usr_paasword.equals("parole123")){
+            System.out.println("Sveiki " + usr_email);
+        } else {
+            System.out.println("Invalid credentials ");
+        }
+    }
+}
+
+// sout + enter uztaisa system out line
---
+++ src/Syst.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
+
+import static com.sun.beans.introspect.PropertyInfo.Name.bound;
+
+public class Syst {
+    public static  void Syst(String[] args) {
+        Random rand = new Random();
+        Scanner scan = new Scanner();
+
+        int numberToGuess = rand.nextInt(bound: 5);
+
+    }
+
+}
